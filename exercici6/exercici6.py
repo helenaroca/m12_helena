0848efbb7cda8bdc6ee4870489da06072d6a771d
hroca <06helena.roca.cat>
2024-01-10 19:06:33 +0100

    hola

---
+++ exercici6/exercici6.py
@@ -0,0 +1,13 @@
+def separador(exercici, descripcio = None):
+    print("\n")
+    print("#" * 25)
+    print("#")
+    print("# Exercici " + str(exercici))
+    print("# Descripció " + str(descripcio)) if descripcio else False
+    print("#")
+    print("#" * 25)
+    print("\n")
+# ############################### 
+
+# ###############################
+separador(1,"Escribe un programa que lea un número natural y ejecute los pasos anteriores siempre que c0 sea diferente de 1")
